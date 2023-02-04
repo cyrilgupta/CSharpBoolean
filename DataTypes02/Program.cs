e8b7f3d8d25512ff8945dacc4bfea17d9b5c312e
Cyril Gupta <5773498+cyrilgupta@users.noreply.github.com>
2023-02-04 21:18:34 +0400

    update

---
+++ DataTypes02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+bool doWeRock = true;
+
+Console.WriteLine(doWeRock);
+
+doWeRock = false;
+
+Console.WriteLine(doWeRock);
+
+if(doWeRock != true)
+{
+    Console.WriteLine("Yaay! We Rock!");
+}
+
+bool doWeRule = true;
+
+if(doWeRock == doWeRule)
+{
+    Console.WriteLine("doWeRock is same as doWeRule");
+}
+else
+{
+    Console.WriteLine("doWeRock is not as same as doWeRule");
+}
+
+//Numerical data types
+
